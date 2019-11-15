a5daf23d91ea7f53226c2baf636ad9600a873ad6
Egoluback <46494602+Egoluback@users.noreply.github.com>
2019-11-15 06:59:20 +0300

    Initial commit

---
+++ PushNotifications/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Security.Principal;
+
+using Microsoft.Win32;
+
+namespace PushNotifications
+{
+    class AutoRun {
+        private string name;
+
+        public AutoRun(String name) {
+            this.name = name;
+        }
+        public bool Run(bool autorun)
+            {
+                string ExePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                RegistryKey reg;
+                reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+                try
+                {
+                    if (autorun) reg.SetValue(name, ExePath);
+                    else reg.DeleteValue(name);
+
+                    reg.Close();
+                }
+                catch
+                {
+                    return false;
+                }
+                return true;
+            }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                AutoRun autoRun = new AutoRun("PushNotifications");
+                autoRun.Run(true);
+            }
+            catch (Exception e) {
+                Console.WriteLine("Catched exception:" + e.Message + "... I'll continue the work.");
+            }
+
+            String host = System.Net.Dns.GetHostName();
+            System.Net.IPAddress ip = System.Net.Dns.GetHostByName(host).AddressList[0];
+
+            String userName = WindowsIdentity.GetCurrent().Name;
+
+
+            string result;
+            string url = @"http://wirepusher.com/send?id=Xh3bmpgWB&title=" + ip.ToString() + "%20launched&message=Pay%20attention%20:" + "%20" + userName + "%20launched%20PC&type=RunComputer";
+
+            try
+            {
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
---
+++ PushNotifications/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("web requests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("web requests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("82ab3b56-5aa1-4eff-afd5-13b70df4e427")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
